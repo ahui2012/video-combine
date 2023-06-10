d8826ab04ca743e98dc4a7458adfd4b58edf0d7b
Terry <ahui8850@live.com>
2023-06-10 16:04:06 +0800

    version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
 
-void main() {
+var supprtedExt = [".mp4", ".mkv", ".avi"];
+List<String> files = <String>[];
+String message = "Please drag video files to this app";
+
+Future<void> main() async {
   runApp(const MyApp());
 }
 
@@ -11,42 +19,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Video Combine',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Video Combine Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +39,179 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _fileDropDone(detail) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      for (var file in detail.files) {
+        if (supprtedExt.contains(p.extension(file.path))) {
+          files.add(file.path);
+        }
+      }
     });
+  }
+
+  void _mergeFile() async {
+    if (files.isEmpty) {
+      return;
+    }
+    var workDir = p.dirname(files[0]);
+    var fileName = p.basename(files[0]);
+    var ext = p.extension(files[0]);
+    var newFileName = fileName.replaceFirst(ext, "-output.mp4");
+    var outputPath = p.join(workDir, newFileName);
+
+    if (File(outputPath).existsSync()) {
+      File(outputPath).deleteSync();
+    }
+
+    var listFile = File(p.join(workDir, "list-tmp.txt"));
+    if (listFile.existsSync()) {
+      listFile.deleteSync();
+    }
+
+    listFile.createSync();
+    var sink = listFile.openWrite();
+    for (var f in files) {
+      sink.writeln("file $f".replaceAll("\\", "/"));
+    }
+    await sink.flush();
+    await sink.close();
+
+    setState(() {
+      message = "waiting...";
+    });
+
+    var args = [
+      "-f",
+      "concat",
+      "-safe",
+      "0",
+      "-i",
+      "list-tmp.txt",
+      "-c",
+      "copy",
+      newFileName
+    ];
+    var result = Process.runSync("ffmpeg", args, workingDirectory: workDir);
+
+    setState(() {
+      if (result.exitCode == 0) {
+        message = "success saved to $outputPath";
+      } else {
+        message = result.stderr.toString();
+      }
+    });
+
+    listFile.deleteSync();
+  }
+
+  void _clearFile() {
+    setState(() {
+      files.clear();
+    });
+  }
+
+  void _fileClicked(int index, String op) {
+    setState(() {
+      if (op == 'delete') {
+        files.removeAt(index);
+      } else if (index > 0 && op == 'move_up') {
+        String tmp = files[index - 1];
+        files[index - 1] = files[index];
+        files[index] = tmp;
+      } else if (index < (files.length - 1) && op == 'move_down') {
+        String tmp = files[index + 1];
+        files[index + 1] = files[index];
+        files[index] = tmp;
+      }
+    });
+  }
+
+  ListTile tileBuilder(BuildContext context, int index) {
+    return ListTile(
+        title: Text(files[index]),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.move_up,
+                size: 20.0,
+                color: Colors.blue[900],
+              ),
+              onPressed: () {
+                _fileClicked(index, 'move_up');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.move_down,
+                size: 20.0,
+                color: Colors.blue[900],
+              ),
+              onPressed: () {
+                _fileClicked(index, 'move_down');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.delete_outline,
+                size: 20.0,
+                color: Colors.red[900],
+              ),
+              onPressed: () {
+                _fileClicked(index, 'delete');
+              },
+            ),
+          ],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    const EdgeInsets margin =
+        EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 50.0);
+    return DropTarget(
+        onDragDone: _fileDropDone,
+        child: Scaffold(
+          body: Container(
+              margin: margin,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 8,
+                        child: ListView.builder(
+                            itemCount: files.length, itemBuilder: tileBuilder)),
+                    Expanded(
+                      flex: 2,
+                      child: Text(message),
+                    ),
+                  ],
+                ),
+              )),
+          floatingActionButton: Wrap(
+            //will break to another line on overflow
+            direction: Axis.horizontal, //use vertical to show  on vertical axis
+            children: <Widget>[
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: FloatingActionButton(
+                    tooltip: 'Clear',
+                    onPressed: _clearFile,
+                    backgroundColor: Colors.blue,
+                    child: const Icon(Icons.clear),
+                  )),
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: FloatingActionButton(
+                    tooltip: 'Merge',
+                    onPressed: _mergeFile,
+                    backgroundColor: Colors.blue,
+                    child: const Icon(Icons.task_alt),
+                  )),
+            ],
+          ),
+        ));
   }
 }
